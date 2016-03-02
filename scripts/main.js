28e2ee16efb2f9fc791f96eededf62a1baeada9e
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-03-02 13:23:10 -0600

    modified contact/detail templates

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+// Initialize collapse button
+$('.button-collapse').sideNav({
+    menuWidth: 150,
+    edge: 'right',
+    closeOnClick: true
+  }
+);
+
+// Initialize textarea
+$('#textarea1').val('New Text');
+$('#textarea1').trigger('autoresize');
+
+// Initialize carousel of technology icons
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
